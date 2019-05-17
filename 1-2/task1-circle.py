9b5226c40a6d23f508c498704f6ad241a49bc3a4
kisa002 <vnycall74@naver.com>
2019-05-17 18:18:42 +0900

    반지름 제곱 오류 수정

--- 1-2/task1-circle.py
+++ 1-2/task1-circle.py
@@ -9,7 +9,7 @@ class Circle(object):
         self.cy = cy
 
     def area(self):
-        return float(self.radius ^ 2) * 3.14
+        return float(self.radius * self.radius) * 3.14
 
     def center(self):
         return "position: {0}, {1}".format(self.cx, self.cy)
